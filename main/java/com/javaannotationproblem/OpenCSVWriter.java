6cb8d5f489c14c647e2e2891ef37a8bf3be2c83d
ShivKokkula <shivbk987@gmail.com>
2021-05-31 22:34:43 +0530

    Refactor - added writing to CSV code

---
+++ main/java/com/javaannotationproblem/OpenCSVWriter.java
@@ -0,0 +1,18 @@
+package com.javaannotationproblem;
+
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class OpenCSVWriter {
+    private static final String SAMPLE_CSV_FILE_PATH = "C:/Users/sunny/IdeaProjects/JavaAnnotationProblems/src/main/resources/user.csv";
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
